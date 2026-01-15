c5ed4c6bed0fbb3d7620a211249867e439b52883
Dzaky Perwira Yasig <dzakyperwira1@gmail.com>
2026-01-15 09:22:55 +0700

    add link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,16 @@
 
     <!-- NAVBAR -->
     <header class="navbar">
-        <div class="container">
-            <h1 class="logo">CRUD Barang</h1>
-        </div>
-    </header>
+    <div class="container">
+        <h1 class="logo">CRUD Barang</h1>
+
+        <nav>
+            <a href="{{ route('barang.index') }}">Barang</a> |
+            <a href="{{ route('supplier.index') }}">Supplier</a>
+        </nav>
+    </div>
+</header>
+
 
     <!-- CONTENT -->
     <main class="container content">
